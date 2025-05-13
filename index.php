debdd8c9de1467b562e1a6ddf3f1b9cdd52a9b26
marwanolwan <52917889+marwanolwan@users.noreply.github.com>
2025-05-13 10:35:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include 'includes/header.php';
+include 'parts/banner.php';       
+include 'parts/feature.php';    // قسم الهيرو (البداية)
+include 'parts/about.php';          // قسم من نحن (الذي جعلناه ديناميكياً)
+include 'parts/products.php';        // قسم العملاء
+include 'parts/contact-page.php';   // قسم آراء العملاء
+include 'includes/footer.php';
+
+?>
