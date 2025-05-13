0d8368b1fd68b462ce0af2000c3c18c34c9b1e55
marwanolwan <52917889+marwanolwan@users.noreply.github.com>
2025-05-13 10:48:39 +0300

    Add files via upload

---
+++ parts/about.php
@@ -0,0 +1,54 @@
+			<!-- Start About us Area -->
+             <p></p>
+			<section class="course-mission-area section-gap-1"  >
+                
+                				<div class="container section-gap-100">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content pb-20 col-lg-8">
+							<div class="title text-center">
+								<h1 class="mb-10">Goals to Achieve for the leadership</h1>
+								<p>Who are in extremely love with eco friendly system.</p>
+							</div>
+						</div>
+					</div>							
+                    <div class="row">
+                        <div class="col-md-6 accordion-left">
+
+                            <!-- accordion 2 start-->
+                            <dl class="accordion">
+                                <dt>
+                                    <a href="">Success</a>
+                                </dt>
+                                <dd>
+                                    Nunc placerat mi id nisi interdum mollis. Praesent pharetra, justo ut scelerisque mattis, leo quam aliquet diam, congue laoreet elit metus eget diam. Proin ac metus diam.
+                                </dd>
+                                <dt>
+                                    <a href="">Info</a>
+                                </dt>
+                                <dd>
+                                    Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. leo quam aliquet diam, congue laoreet elit metus eget diam.
+                                </dd>
+                                <dt>
+                                    <a href="">Danger</a>
+                                </dt>
+                                <dd>
+                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. Proin ac metus diam.
+                                </dd>
+                                <dt>
+                                    <a href="">Warning</a>
+                                </dt>
+                                <dd>
+                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. Proin ac metus diam.
+                                </dd>                                    
+                            </dl>
+                            <!-- accordion 2 end-->
+                        </div>
+                        <div class="col-md-6 video-right justify-content-center align-items-center d-flex relative">
+                        	<div class="overlay overlay-bg"></div>
+							<a class="play-btn" ><img class="img-fluid mx-auto" src="img/p1.jpg" alt=""></a>
+                        </div>
+
+                    </div>
+				</div>	
+			</section>
+			<!-- End About us Area -->
---
+++ parts/products.php
@@ -0,0 +1,186 @@
+			<!-- start products Area -->
+			 <p></p>
+			 <section class="popular-courses-area courses-page">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content pb-20 col-lg-8">
+							<div class="title text-center">
+								<h1 class="mb-10">Popular Courses we offer</h1>
+								<p>There is a moment in the life of any aspiring.</p>
+							</div>
+						</div>
+					</div>						
+					<div class="row">
+						<div class="single-popular-carusel col-lg-3 col-md-6">
+							<div class="thumb-wrap relative">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>	
+									<img class="img-fluid" src="img/p1.jpg" alt="">
+								</div>
+								<div class="meta d-flex justify-content-between">
+									<p><span class="lnr lnr-users">
+								</div>									
+							</div>
+							<div class="details">
+								<a href="course-details.html">
+									<h4>
+										Learn Designing
+									</h4>
+								</a>
+								<p>
+									When television was young, there was a hugely popular show based on the still popular fictional characte										
+								</p>
+							</div>
+						</div>	
+						<div class="single-popular-carusel col-lg-3 col-md-6">
+							<div class="thumb-wrap relative">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>	
+									<img class="img-fluid" src="img/p2.jpg" alt="">
+								</div>
+								<div class="meta d-flex justify-content-between">
+									<p><span class="lnr lnr-users">
+								</div>									
+							</div>
+							<div class="details">
+								<a href="course-details.html">
+									<h4>
+										Learn React js beginners
+									</h4>
+								</a>
+								<p>
+									When television was young, there was a hugely popular show based on the still popular fictional characte										
+								</p>
+							</div>
+						</div>	
+						<div class="single-popular-carusel col-lg-3 col-md-6">
+							<div class="thumb-wrap relative">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>	
+									<img class="img-fluid" src="img/p3.jpg" alt="">
+								</div>
+								<div class="meta d-flex justify-content-between">
+									<p><span class="lnr lnr-users">
+								</div>									
+							</div>
+							<div class="details">
+								<a href="course-details.html">
+									<h4>
+										Learn Photography
+									</h4>
+								</a>
+								<p>
+									When television was young, there was a hugely popular show based on the still popular fictional characte										
+								</p>
+							</div>
+						</div>	
+						<div class="single-popular-carusel col-lg-3 col-md-6">
+							<div class="thumb-wrap relative">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>	
+									<img class="img-fluid" src="img/p4.jpg" alt="">
+								</div>
+								<div class="meta d-flex justify-content-between">
+									<p><span class="lnr lnr-users">
+								</div>									
+							</div>
+							<div class="details">
+								<a href="course-details.html">
+									<h4>
+										Learn Surveying
+									</h4>
+								</a>
+								<p>
+									When television was young, there was a hugely popular show based on the still popular fictional characte										
+								</p>
+							</div>
+						</div>
+						<div class="single-popular-carusel col-lg-3 col-md-6">
+							<div class="thumb-wrap relative">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>	
+									<img class="img-fluid" src="img/p1.jpg" alt="">
+								</div>
+								<div class="meta d-flex justify-content-between">
+									<p><span class="lnr lnr-users">
+								</div>									
+							</div>
+							<div class="details">
+								<a href="course-details.html">
+									<h4>
+										Learn Designing
+									</h4>
+								</a>
+								<p>
+									When television was young, there was a hugely popular show based on the still popular fictional characte										
+								</p>
+							</div>
+						</div>	
+						<div class="single-popular-carusel col-lg-3 col-md-6">
+							<div class="thumb-wrap relative">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>	
+									<img class="img-fluid" src="img/p2.jpg" alt="">
+								</div>
+								<div class="meta d-flex justify-content-between">
+									<p><span class="lnr lnr-users">
+								</div>									
+							</div>
+							<div class="details">
+								<a href="course-details.html">
+									<h4>
+										Learn React js beginners
+									</h4>
+								</a>
+								<p>
+									When television was young, there was a hugely popular show based on the still popular fictional characte										
+								</p>
+							</div>
+						</div>	
+						<div class="single-popular-carusel col-lg-3 col-md-6">
+							<div class="thumb-wrap relative">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>	
+									<img class="img-fluid" src="img/p3.jpg" alt="">
+								</div>
+								<div class="meta d-flex justify-content-between">
+									<p><span class="lnr lnr-users">
+								</div>									
+							</div>
+							<div class="details">
+								<a href="course-details.html">
+									<h4>
+										Learn Photography
+									</h4>
+								</a>
+								<p>
+									When television was young, there was a hugely popular show based on the still popular fictional characte										
+								</p>
+							</div>
+						</div>	
+						<div class="single-popular-carusel col-lg-3 col-md-6">
+							<div class="thumb-wrap relative">
+								<div class="thumb relative">
+									<div class="overlay overlay-bg"></div>	
+									<img class="img-fluid" src="img/p4.jpg" alt="">
+								</div>
+								<div class="meta d-flex justify-content-between">
+									<p><span class="lnr lnr-users">
+								</div>									
+							</div>
+							<div class="details">
+								<a href="course-details.html">
+									<h4>
+										Learn Surveying
+									</h4>
+								</a>
+								<p>
+									When television was young, there was a hugely popular show based on the still popular fictional characte										
+								</p>
+							</div>
+						</div>								
+						<a href="#" class="primary-btn text-uppercase mx-auto">Load More Courses</a>													
+					</div>
+				</div>	
+			</section>
+			<!-- End products Area -->
